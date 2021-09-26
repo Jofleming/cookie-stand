0494b78184e7fa1cc637b14b222372fcf6d8d73f
Jordan Fleming <jordano.fleming@gmail.com>
2021-09-26 13:30:03 -0700

    finishing up salmon-cookies lab

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let storeHours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12pM', '1PM', '2PM', '3PM', '4PM', '5PM', '6AM', '7PM'];
+let storeHours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6AM', '7PM'];
 let storeLocations = ['Seattle', 'Tokyo', 'Dubai', 'Paris', 'Lima'];
 
 function Store(name, min_cus, max_cus, avgcpp) {
